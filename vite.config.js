460eda7b77a33d3928321001f31fb56fa2396077
Rana Ahmer <ranaahmer4956@example.com>
2025-12-03 22:51:12 +0500

    config file

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,36 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000', // no trailing slash
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, '') // backend /register, /login routes ke liye
+//       }
+//     }
+//   },
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // no trailing slash
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '') // backend /register, /login routes ke liye
+        rewrite: path => path.replace(/^\/api/, '')
       }
     }
   },
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
